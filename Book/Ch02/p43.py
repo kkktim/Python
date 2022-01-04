3503886c926451f47519b9d633afd01e0ffab8e8
unknown <rkdxogh1987@gmail.com>
2022-01-04 15:20:24 +0900

    2022/01/04 Python 교제예제

---
+++ Book/Ch02/p43.py
@@ -0,0 +1,15 @@
+# (1) 문자형 숫자 입력
+num = input("숫자 입력 : ")
+print('num type : ', type(num))     # <class 'str'>
+print('num = ', num)
+print('num = ', num*2)
+
+# (2) 문자형 숫자를 정수형으로 변환
+num1 = int( input("숫자 입력 : "))     # str -> int
+print('num1 = ', num1*2)
+
+
+# (3) 문자형 숫자를 실수형으로 변환
+num3 = float( input('숫자 입력 : '))    # str -> float
+result = num1 + num2
+print('result = ', result)
